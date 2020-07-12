c5e8912405784a1ae91b4039c5680dabf89e8259
fatima-ola <fatimaolasunkanmi01@gmail.com>
2020-07-12 23:58:15 +0100

    Updates

---
+++ precache-manifest.edf049d3de85bafd018145e919236e04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba91d7a18bafc5af2d10f917f907b0a8",
+    "url": "/image-search-app/index.html"
+  },
+  {
+    "revision": "0e604fd9ed3401028a79",
+    "url": "/image-search-app/static/css/main.12fd00dd.chunk.css"
+  },
+  {
+    "revision": "0db2cdf69d27b4f8c0ed",
+    "url": "/image-search-app/static/js/2.99090868.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/image-search-app/static/js/2.99090868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e604fd9ed3401028a79",
+    "url": "/image-search-app/static/js/main.a1f7245e.chunk.js"
+  },
+  {
+    "revision": "99c06ee0b2a71825c2cf",
+    "url": "/image-search-app/static/js/runtime-main.f46805bd.js"
+  }
+]);
